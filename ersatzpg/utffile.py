7d0d97476f196d92bafbc6e1bed1453939122de7
Nathaniel Gaertner <gaertner@gaertner-lap.(none)>
2012-09-27 09:34:32 -0400

    fix for partition load

--- ersatzpg/utffile.py
+++
@@ -1,7 +0,0 @@
-import chardet
-
-class utffile(file):
-    def next(self):
-        n = super(utffile, self).next()
-        d = chardet.detect(n)
-        return n.decode(d['encoding']).encode('utf-8')
