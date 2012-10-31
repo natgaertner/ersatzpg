f9b268f729c8bc318eb865f1a33c17a27ce72cfc
Nathaniel Gaertner <gaertner@gaertner-lap.(none)>
2012-10-30 20:32:22 -0400

    double check that unicode

--- ersatzpg/utffile.py
+++ ersatzpg/utffile.py
@@ -3,5 +3,7 @@ import chardet
 class utffile(file):
     def next(self):
         n = super(utffile, self).next()
+        if type(n) == unicode:
+            return n.encode('utf-8')
         d = chardet.detect(n)
         return n.decode(d['encoding']).encode('utf-8')
